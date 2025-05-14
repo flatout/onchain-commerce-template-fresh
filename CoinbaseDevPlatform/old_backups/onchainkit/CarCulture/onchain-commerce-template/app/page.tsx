d499a7fef4991c7978331f385b144275aae797a6
flatout <cindy.lewis@carculture.com>
2025-05-14 16:22:06 -0400

    Remove grocery-list folder

--- CoinbaseDevPlatform/old_backups/onchainkit/CarCulture/onchain-commerce-template/app/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import CarManiaStore from '../components/CarManiaStore';
-
-export default function Home() {
-  return <CarManiaStore />;
-} 
--- CoinbaseDevPlatform/old_backups/onchainkit/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['ipfs.io', 'gateway.ipfs.io', 'manifold.xyz', 'arweave.net'],
-  },
-}
-
-export default nextConfig;
